171e377afd5283e442880cf7c823204fb4c2bc27
arseniy shalamov <arsenshalamov16@gmail.com>
2023-06-15 20:18:04 +0300

    newHW

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,11 @@
 'use strict';
 
 export default class Validator {
-    static validateUsername(use) {
-        let fal = false;
-        let validate = /^[a-zA-Z]+([a-zA-Z]|[-_]|\d{0,3}?)*[a-zA-Z]+$/;
-
-        fal = validate.test(use);
-        if (fal) {
-            fal = !(/\d{4}/.test(use));
-        }
-        return fal;
+    static validateUsername(username) {
+      const usernameRegex = /^[a-zA-Z0-9_-]+$/;
+      const fourDigitsRegex = /\d{4}/;
+  
+      const isValid = usernameRegex.test(username) && !fourDigitsRegex.test(username);
+      return isValid;
     }
-}
+  }
